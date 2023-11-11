8173e145c6fe488c7d70c2719c71f29f182805f9
Trever O <toveson9522@gmail.com>
2023-11-10 19:36:01 -0700

    added top bar and right navbar to layout

---
+++ app/(pages)/groups/page.tsx
@@ -0,0 +1,4 @@
+const GroupsPage = () => {
+    return <div>groups page</div>;
+  };
+  export default GroupsPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Stack, Button } from "@mui/material";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Adventure',
-  description: 'App to organize and plan your next solo or group adventure.',
+  title: "+VNTR",
+  description: "App to organize and plan your next solo or group adventure.",
 };
 
 export default function RootLayout({
@@ -11,9 +13,85 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const buttons = [
+    { label: "Dashboard", href: "/dashboard" },
+    { label: "Trips", href: "/trips" },
+    { label: "Achievements", href: "/achievements" },
+    { label: "Friends", href: "/friends" },
+    { label: "Groups", href: "/groups" },
+    { label: "Chat", href: "/chat" },
+    { label: "Events", href: "/events" },
+  ];
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <CssBaseline />
+      <html lang="en">
+        <body
+          style={{
+            margin: 0,
+            padding: 0,
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Stack
+            spacing={2}
+            style={{ backgroundColor: "#000000", height: "50px" }}
+          >
+            <Stack direction="row">Logo goes here</Stack>
+          </Stack>
+          <Stack
+            direction="row"
+            style={{ justifyContent: "space-between", flex: 1 }}
+          >
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              style={{ flex: 1 }} // Added flex: 1 to center the children Stack
+            >
+              {children}
+            </Stack>
+            <Stack
+              spacing={1}
+              alignItems="center"
+              style={{
+                backgroundColor: "#542F12",
+                width: "180px",
+                justifyContent: "space-between",
+              }}
+            >
+              <Stack spacing={1} style={{ paddingTop: "15px" }}>
+                {buttons.map((button) => (
+                  <Link href={button.href} passHref key={button.label}>
+                    <Button
+                      style={{
+                        backgroundColor: "#4E8448",
+                        color: "#000000",
+                        width: "150px",
+                      }}
+                    >
+                      {button.label}
+                    </Button>
+                  </Link>
+                ))}
+              </Stack>
+              <Stack style={{ paddingBottom: "15px" }}>
+                <Button
+                  style={{
+                    backgroundColor: "#4E8448",
+                    color: "#000000",
+                    width: "150px",
+                  }}
+                >
+                  Sign Out
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </body>
+      </html>
+    </>
   );
 }
