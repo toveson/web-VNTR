cbbbdf913f16e70a42327ce7fabf2227741e3b9a
JacekHacking <Jacekhacking@mac.com>
2023-11-14 22:33:20 -0700

    basic conditional rendering working

--- app/(pages)/trips/[tripId]/page.tsx
+++ app/(pages)/trips/[tripId]/page.tsx
@@ -9,6 +9,23 @@ import {
   ShoppingList,
   PackingList,
 } from './index';
+import { Shop } from '@mui/icons-material';
+
+const renderTripDetail = (page: string) => {
+  switch (page) {
+    case 'Info':
+      return <TripsInfo />;
+
+    case 'Shopping':
+      return <ShoppingList />;
+
+    case 'Packing':
+      return <PackingList />;
+
+    case 'Chat':
+      return <Chat />;
+  }
+};
 
 export default function TripDetailPage() {
   const [tab, setTab] = useState<string>('Info');
@@ -16,7 +33,7 @@ export default function TripDetailPage() {
   return (
     <Stack style={{ flexGrow: 1, background: '#F8EBDE' }}>
       <TripDetailHeader tab={tab} setTab={setTab} />
-      <TripsInfo />;
+      {renderTripDetail(tab)}
     </Stack>
   );
 }
