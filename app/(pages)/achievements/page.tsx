129121680b85d5e38a2987ba337ddeeda278b5cb
EsotericZ <cjsand03@gmail.com>
2023-11-14 17:51:15 -0700

    added styling to the earned chevos cards

--- app/(pages)/achievements/page.tsx
+++ app/(pages)/achievements/page.tsx
@@ -3,10 +3,13 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Grid,
   IconButton,
-  Stack, 
+  Tooltip,
+  Typography,
 } from "@mui/material";
 import ShareIcon from '@mui/icons-material/Share';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 const testChevos = [
   {
@@ -23,43 +26,74 @@ const testChevos = [
     timesAchieved: 2,
     lastAchieved: "Sept 1, 2022",
   },
+  {
+    name: "First Login", 
+    description: "You Did It, Look at you",
+    image: "https://cdn2.iconfinder.com/data/icons/people-79/100/Goal-04-512.png",
+    timesAchieved: 1,
+    lastAchieved: "Sept 1, 2022",
+  },
+  {
+    name: "Winning The World", 
+    description: "You Did It",
+    image: "https://cdn2.iconfinder.com/data/icons/people-79/100/Goal-04-512.png",
+    timesAchieved: 2,
+    lastAchieved: "Sept 1, 2022",
+  },
+  {
+    name: "First Login", 
+    description: "You Did It, Look at you",
+    image: "https://cdn2.iconfinder.com/data/icons/people-79/100/Goal-04-512.png",
+    timesAchieved: 1,
+    lastAchieved: "Sept 1, 2022",
+  },
 ];
 
 export default function Achievements() {
   return (
-    <Stack 
-      style={{ backgroundColor: "#F8EBDE", flexGrow: 1 }} 
-      direction="row"
+    <Grid 
+      container
+      style={{ backgroundColor: "#F8EBDE", flexGrow: 1, margin: 0 }} 
       justifyContent="center"
       alignItems="center"
-      spacing={2}
+      spacing={3}
     >
       {testChevos.map((chevo, index) => {
         return (
-          <Card 
-            key={index}
-            sx={{ width: 250 }}
-          >
-            <CardContent
-              sx={{ textAlign: "center" }}
+          <Grid item>
+            <Card 
+              key={index}
+              sx={{ width: 250, backgroundColor: "#4E8448" }}
+              raised={true}
             >
-              {chevo.name}
-            </CardContent>
-            <CardMedia
-              component="img"
-              height="150"
-              image={chevo.image}
-              alt={chevo.name}
-              sx={{ objectFit: "contain" }}
-            />
-            <CardActions>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
+              <CardContent sx={{ textAlign: "center" }} >
+                <Typography sx={{ color: "#F8EBDE", fontWeight: "bold" }} >
+                  {chevo.name}
+                </Typography>
+              </CardContent>
+              <CardMedia
+                component="img"
+                height="200"
+                image={chevo.image}
+                alt={chevo.name}
+                sx={{ objectFit: "contain", borderRadius: "50%", border: "5px solid #542F12", backgroundColor: "#73C7E7", maxWidth: "200px", margin: "auto"}}
+              />
+              <CardActions disableSpacing sx={{ justifyContent: "space-between"}}>
+                <Tooltip title="Share">
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="More">
+                  <IconButton aria-label="more">
+                    <MoreHorizIcon />
+                  </IconButton>
+                </Tooltip>
+              </CardActions>
+            </Card>
+          </Grid>
         )
       })}
-    </Stack>
+    </Grid>
   );
 };
