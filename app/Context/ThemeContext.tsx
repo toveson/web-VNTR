09595a1eb3488e5887b1c24152724128aff70f19
Trever O <toveson9522@gmail.com>
2023-11-14 23:40:56 -0700

    added our colors hex codes to ThemeContext

--- app/Context/ThemeContext.tsx
+++ app/Context/ThemeContext.tsx
@@ -2,6 +2,14 @@
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ReactNode, FC, useContext, createContext } from "react";
+
+//* Our color theme
+//* Black: 000000
+//* Fern Green: 4E8448
+//* Seal Brown: 542F12
+//* Sky Blue: 73C7E7
+//* Linen: F8EBDE
+
 const theme = createTheme({
   palette: {
     primary: {
