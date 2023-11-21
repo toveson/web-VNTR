d3f59a593a4311b85ff5e8ba6c9c44f716f5fb42
JacekHacking <Jacekhacking@mac.com>
2023-11-20 20:29:39 -0700

    able to login and get user information with github

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,9 +1,19 @@
-import { Stack } from "@mui/material";
+'use client';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { Stack } from '@mui/material';
 
 export default function Dashboard() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>error.message</div>;
+  console.log(user);
   return (
-    <Stack style={{ backgroundColor: "#F8EBDE", flexGrow: 1 }}>
-      Hello From Dashboard
-    </Stack>
+    user && (
+      <Stack style={{ backgroundColor: '#F8EBDE', flexGrow: 1 }}>
+        Hello {user.name}
+        <a href="/api/auth/logout">Logout</a>
+      </Stack>
+    )
   );
 }
